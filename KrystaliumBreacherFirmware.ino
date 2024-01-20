12b2cfaf66b777a6c99b96a4bfd94a59c25a5d29
Jaime van Kessel <nallath@gmail.com>
2024-01-20 17:12:47 +0100

    Add flickering which is influenced by distance
    Pretty much the last bit to make it work properly. Huzzah!

--- KrystaliumBreacherFirmware.ino
+++ KrystaliumBreacherFirmware.ino
@@ -15,7 +15,71 @@ namedMesh  mesh;
 
 Kalman myFilter(0.15, 20, 20, -50.);
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, D1, NEO_GRB + NEO_KHZ800);
+
+// Number of pixels in the ledring
+#define NUMPIXELS                       12 
+// If set to 1, it will consider all pixels in the ring as the same. If set to the same number, it will animate each of them individually
+#define NUM_LED_GROUPS                  12 
+
+// Any unconnected pin, to try to generate a random seed
+#define UNCONNECTED_PIN                 2
+#define LEDRINGPIN                      D2 
+ 
+// The LED can be in only one of these states at any given time
+#define BRIGHT                          0
+#define UP                              1
+#define DOWN                            2
+#define DIM                             3
+#define BRIGHT_HOLD                     4
+#define DIM_HOLD                        5
+
+
+// Percent chance the LED will suddenly fall to minimum brightness
+#define FLICKER_BOTTOM_PERCENT          10
+// Absolute minimum of the flickering
+#define FLICKER_ABSOLUTE_MIN_INTENSITY  32
+// Minimum intensity during "normal" flickering (not a dramatic change)
+#define FLICKER_MIN_INTENSITY           128
+// Maximum intensity of the flickering
+#define FLICKER_MAX_INTENSITY           255
+
+
+// Decreasing brightness will take place over a number of milliseconds in this range
+#define DOWN_MIN_MSECS                  20
+#define DOWN_MAX_MSECS                  250
+// Increasing brightness will take place over a number of milliseconds in this range
+#define UP_MIN_MSECS                    20
+#define UP_MAX_MSECS                    250
+// Percent chance the color will hold unchanged after brightening
+#define BRIGHT_HOLD_PERCENT             20
+// When holding after brightening, hold for a number of milliseconds in this range
+#define BRIGHT_HOLD_MIN_MSECS           0
+#define BRIGHT_HOLD_MAX_MSECS           100
+// Percent chance the color will hold unchanged after dimming
+#define DIM_HOLD_PERCENT                5
+// When holding after dimming, hold for a number of milliseconds in this range
+#define DIM_HOLD_MIN_MSECS              0
+#define DIM_HOLD_MAX_MSECS              50
+
+// Mixes in a certain amount of red in every group.
+#define MIN_IMPURITY                    32
+#define MAX_IMPURITY                    96
+ 
+#define MINVAL(A,B)             (((A) < (B)) ? (A) : (B))
+#define MAXVAL(A,B)             (((A) > (B)) ? (A) : (B))
+
+
+#define MAX_DISTANCE_METER               12
+
+byte state[NUM_LED_GROUPS];
+unsigned long flicker_msecs[NUM_LED_GROUPS];
+unsigned long flicker_start[NUM_LED_GROUPS];
+byte index_start[NUM_LED_GROUPS];
+byte index_end[NUM_LED_GROUPS];
+byte impurity[NUM_LED_GROUPS];
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LEDRINGPIN, NEO_GRB + NEO_KHZ800);
+
 
 // Prototype functions
 void sendMessage() ; 
@@ -37,6 +101,8 @@ float distance_factor = 3.5;
 double rssi_average = -50.0;
 double last_wifi_rssi = -50;
 
+double intensity_factor = 0; // Goes from 0 to 1, defined by how far the device is away from the generator
+
 float getDistance(const float rssi)
 { 
   return pow(10, (reference_power - rssi) / (10 * distance_factor)); 
@@ -44,7 +110,6 @@ float getDistance(const float rssi)
 
 void printWifiRSSI()
 {
-
     //Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");
 }
 
@@ -58,38 +123,9 @@ void calculateDistanceFromRSSI()
     // DO NOTHING!
     return;
   }
-  int r_factor = 0;
-  int g_factor = 0;
-
-  String parent_name = findName(painlessmesh::tcp::encodeNodeId(WiFi.BSSID()));
-
-  if(parent_name == "generator")
-  {
-    r_factor = 0;
-    g_factor = 1;
-  } else
-  {
-    r_factor = 1;
-    g_factor = 0;
-  }
-
-  if(WiFi.status() != WL_CONNECTED)
-  {
-    r_factor = 0;
-    g_factor = 0;
-  }
-
-  
-  for(int i = 0; i < 12; i++)
-  {
-    if(i < getDistance(rssi_average))
-    {
-      strip.setPixelColor(i, r_factor * 80, g_factor * 80, 0); 
-    } else
-    {
-      strip.setPixelColor(i,0,0,0);
-    } 
-  }
+  intensity_factor = MAXVAL(MINVAL(getDistance(rssi_average) / MAX_DISTANCE_METER, 1), 0);
+  Serial.print("Intensity factor is :");
+  Serial.println(intensity_factor);
   strip.show();
 }
 
@@ -112,6 +148,60 @@ void printSubConnection()
   taskPrintSubConnection.setInterval(TASK_SECOND * 10);
 }
 
+void setFlickerIntensity(byte intensity, int group_index)
+{
+  int led_index = group_index * (NUMPIXELS / NUM_LED_GROUPS);
+  int max_led_index = led_index + (NUMPIXELS / NUM_LED_GROUPS);
+  int secondary_intensity;
+  int impurity_base = impurity[group_index] / 2;
+  int impurity_intensity = impurity_base + (impurity_base * ((float)intensity / (float)FLICKER_MAX_INTENSITY));
+  
+  // Clamp intensity between max and absolute min.
+  intensity = MAXVAL(MINVAL(intensity, FLICKER_MAX_INTENSITY), FLICKER_ABSOLUTE_MIN_INTENSITY);
+
+  if (intensity >= FLICKER_MIN_INTENSITY)
+  {
+    secondary_intensity = intensity * 3 / 8; 
+  } else {
+    secondary_intensity = intensity * 3.25 / 8;
+  }
+
+  // Scale all the intensities on the factor.
+  intensity *= intensity_factor;
+  secondary_intensity *= intensity_factor;
+  impurity_intensity *= intensity_factor;
+  
+  for(; led_index < max_led_index; led_index++)
+  {
+    strip.setPixelColor(led_index, impurity_intensity, secondary_intensity, intensity); 
+  }
+    
+  strip.show();
+  return;
+}
+
+void setupNeoPixel()
+{
+  // There is no good source of entropy to seed the random number generator,
+  // so we'll just read the analog value of an unconnected pin.  This won't be
+  // very random either, but there's really nothing else we can do.
+  //
+  // True randomness isn't strictlyfor(int group_index necessary, we just don't want a whole
+  // string of these things to do exactly the same thing at the same time if
+  // they're all powered on simultaneously.
+  randomSeed(analogRead(UNCONNECTED_PIN));
+  Serial.println("SETTING UP NEOPIXEL");
+  for(int group_index = 0; group_index < NUM_LED_GROUPS; group_index++)
+  {
+    setFlickerIntensity(255, group_index);
+    index_start[group_index] = 255;
+    index_end[group_index] = 255;
+    setFlickerState(BRIGHT, group_index);
+    impurity[group_index] = int (random(MIN_IMPURITY, MAX_IMPURITY) + 0.5);
+  }
+  strip.begin();
+}
+
 String findName(uint32_t node_id)
 {
   // Yeaaah i know. It's a bit ugly hardcoded, but it's for a quick test.
@@ -129,7 +219,8 @@ String findName(uint32_t node_id)
 }
 
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
   //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
@@ -151,7 +242,6 @@ void setup() {
     Serial.println("Starting in WIFI_STA mode");
     mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_STA);
   }
-
 
   //mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   nodeName = findName(mesh.getNodeId());
@@ -197,10 +287,98 @@ void setup() {
     Serial.println("This device is not setup to be root");
   }
 
-  strip.begin();
+  setupNeoPixel();
 }
 
 void loop() {
   // it will run the user scheduler as well
   mesh.update();
+  neopixelLoop();
+}
+
+
+void neopixelLoop()
+{
+  unsigned long current_time; 
+  current_time = millis();
+  //Serial.println("loop!");
+  for(int group_index = 0; group_index < NUM_LED_GROUPS; group_index++)
+  {
+    switch (state[group_index])
+    {
+      case BRIGHT:
+      {   
+        //Serial.println("Bright"); 
+        flicker_msecs[group_index] = random(DOWN_MAX_MSECS - DOWN_MIN_MSECS) + DOWN_MIN_MSECS;
+        flicker_start[group_index] = current_time;
+        index_start[group_index] = index_end[group_index];
+        if (index_start[group_index] > FLICKER_ABSOLUTE_MIN_INTENSITY && random(100) < FLICKER_BOTTOM_PERCENT)
+        {
+          index_end[group_index] = random(index_start[group_index] - FLICKER_ABSOLUTE_MIN_INTENSITY) + FLICKER_ABSOLUTE_MIN_INTENSITY;
+        } else {
+          index_end[group_index] = random(index_start[group_index]- FLICKER_MIN_INTENSITY) + FLICKER_MIN_INTENSITY;
+        }
+   
+        setFlickerState(DOWN, group_index);
+        break;  
+      }  
+      case DIM:
+      {
+        //Serial.println("Dim");
+        flicker_msecs[group_index] = random(UP_MAX_MSECS - UP_MIN_MSECS) + UP_MIN_MSECS;
+        flicker_start[group_index] = current_time;
+        index_start[group_index] = index_end[group_index];
+        index_end[group_index] = random(FLICKER_MAX_INTENSITY - index_start[group_index]) + FLICKER_MIN_INTENSITY;
+        setFlickerState(UP, group_index);
+        break;
+      }
+      case BRIGHT_HOLD:  
+      case DIM_HOLD:
+      {
+        //Serial.println("DIM Hold");
+        if (current_time >= (flicker_start[group_index] + flicker_msecs[group_index]))
+        {
+          setFlickerState(state[group_index] == BRIGHT_HOLD ? BRIGHT : DIM, group_index); 
+        }
+        break;
+      }
+      case UP:
+      case DOWN:
+      {
+        //  Serial.println("Down");
+        if (current_time < (flicker_start[group_index] + flicker_msecs[group_index])) {
+          setFlickerIntensity(index_start[group_index] + ((index_end [group_index]- index_start[group_index]) * (((current_time - flicker_start[group_index]) * 1.0) / flicker_msecs[group_index])), group_index);
+        } else {
+          setFlickerIntensity(index_end[group_index], group_index);
+   
+          if (state[group_index] == DOWN)
+          {
+            if (random(100) < DIM_HOLD_PERCENT)
+            {
+              flicker_start[group_index] = current_time;
+              flicker_msecs[group_index] = random(DIM_HOLD_MAX_MSECS - DIM_HOLD_MIN_MSECS) + DIM_HOLD_MIN_MSECS;
+              setFlickerState(DIM_HOLD, group_index);
+            } else {
+              setFlickerState(DIM, group_index);
+            } 
+          } else {
+            if (random(100) < BRIGHT_HOLD_PERCENT)
+            {
+              flicker_start[group_index] = current_time;
+              flicker_msecs[group_index] = random(BRIGHT_HOLD_MAX_MSECS - BRIGHT_HOLD_MIN_MSECS) + BRIGHT_HOLD_MIN_MSECS;
+              setFlickerState(BRIGHT_HOLD, group_index);
+            } else {
+              setFlickerState(BRIGHT,group_index);
+            }
+          }
+        }
+        break;
+      }
+    }
+  }
+}
+
+void setFlickerState(byte new_state, int group_index)
+{
+  state[group_index] = new_state;
 }
